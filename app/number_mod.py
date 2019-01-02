8de56c53e016519727376e5022e606c6d346d637
Johannes Esbjornsson <johannes.esbjornsson@gmail.com>
2019-01-02 19:55:02 +0000

    fail test

--- app/number_mod.py
+++ app/number_mod.py
@@ -3,6 +3,6 @@ import random
 def get_some_numbers():
   numbers = []
   for x in range(100):
-    random_int = random.randint(0,25)
+    random_int = random.randint(0,27)
     numbers.append(random_int)
   return numbers
