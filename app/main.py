d3c8d8bf03c75d4c5571b21ec547c3a129116a75
Johannes Esbjörnsson <johannesesbjornsson@Johanness-MacBook-Pro.local>
2019-01-09 10:17:19 +0100

    changing response

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello():
     numbs = number_mod.get_some_numbers()
     string = letter_mod.get_some_letters(numbs)
-    return "Oi!! Look at these sick, randomly generated letters. Pretty cool, hey?  "+ string
+    return "Oi! Look at these sick, randomly generated letters. Pretty cool, hey?  "+ string
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)
